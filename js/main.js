06ccf889e915ccd90d0ca677103c3fbb037eec71
Shaurya Chandhoke <chandhoke.shaurya@gmail.com>
2022-02-01 01:52:02 -0500

    Finished first draft of personal portfolio website

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@
 			scaleColor: false,
 			lineWidth: 4,
 			lineCap: 'butt',
-			barColor: '#FF9000',
+			barColor: 'rgba(61, 121, 164)',
 			trackColor:	"#f5f5f5",
 			size: 160,
 			animate: 1000
