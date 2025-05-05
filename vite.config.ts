500c58df2f55bf35c87d4244e0ddaf4320efbc8b
Shaurya Chandhoke <chandhoke.shaurya@gmail.com>
2025-05-05 19:54:58 -0400

    feat: Pivoting to GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'docs' // <--- this is the key change
-    }
+    plugins: [react()]
 })
